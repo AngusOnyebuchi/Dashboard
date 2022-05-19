21d3f2e7eb9c2152d6091a3838af392d8dc84159
AngusOnyebuchi <66187866+AngusOnyebuchi@users.noreply.github.com>
2022-05-19 20:18:15 +0200

    Navlink commit

---
+++ .history/src/components/Team_20220519201621.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+export default function Team(props) {
+    
+  return (
+    <div name="team" className='ml-48'>
+
+        <div className='px-8'>
+        
+        {/** The Team Container/card */}
+        <div className='text-black'>
+              <div className='mb-8 mt-4'>
+                  <p><strong>Full name:</strong> {props.firstname} {props.lastname}</p>
+                  <p><strong>Email:</strong> {props.email}</p>
+                  <p><strong>Phone number:</strong> {props.phone}</p>
+                  <p><strong>Gender:</strong> {props.gender}</p>
+                  <img src={props.image} alt='' className='mt-2 h-48'/>
+              </div>
+        </div>
+
+        </div>
+
+    </div>
+  )
+}
